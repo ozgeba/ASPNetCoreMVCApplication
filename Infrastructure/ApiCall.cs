fa992860c9d70695aa515d6cd5851ffb6a626c4e
özge baykara sarı <baykaraozge@gmail.com>
2020-10-25 21:34:26 +0300

    Add project files.

---
+++ Infrastructure/ApiCall.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using System;
+
+namespace Infrastructure
+{
+    public class ApiCall
+    {
+        public static string GetValueById(int i)
+        {
+            var client = new RestClient(string.Format("http://localhost:54802/api/values/{0}", i));
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+    }
+}
